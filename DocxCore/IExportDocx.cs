f0accdbaddbd66159a14088d85b94da5a7ca42c2
LiuJiaCheng <1311804336@qq.com>
2021-09-08 11:50:59 +0800

    修复Core文件

---
+++ DocxCore/IExportDocx.cs
@@ -0,0 +1,152 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Docx.Core
+{
+    /// <summary>
+    /// 导出Docx文件的参数
+    /// </summary>
+    /// <summary>
+    /// 导出Docx文件的参数
+    /// </summary>
+    public interface IExportDocx
+    {
+        /// <summary>
+        /// 表示此处应重复替换，并换行显示，此符号前面的为数组，此符号后面的为数组的字段，形如{{XX:YY}}
+        /// </summary>
+        string RangeSplitText { get; set; }
+        /// <summary>
+        /// 表示此处有特性，
+        /// </summary>
+        string AttributeSplitText { get; set; }
+        /// <summary>
+        /// 匹配普通文本
+        /// </summary>
+        string TextLeft { get; set; }
+        /// <summary>
+        /// 匹配普通文本
+        /// </summary>
+        string TextRight { get; set; }
+        /// <summary>
+        /// 匹配表格文本
+        /// </summary>
+        string TableTextLeft { get; set; }
+        /// <summary>
+        /// 匹配表格文本
+        /// </summary>
+        string TableTextRight { get; set; }
+        /// <summary>
+        /// 获取匹配的正则表达式，一般不需要重写
+        /// </summary>
+        /// <returns></returns>
+        Regex GetTextRegex();
+        /// <summary>
+        /// 获取匹配的内容的正则表达式，一般不需要重写
+        /// </summary>
+        /// <returns></returns>
+        Regex GetInnerTextRegex();
+        /// <summary>
+        /// 获取匹配的正则表达式，一般不需要重写
+        /// </summary>
+        /// <returns></returns>
+        Regex GetTableTextRegex();
+        /// <summary>
+        /// 获取匹配的内容的正则表达式，一般不需要重写
+        /// </summary>
+        /// <returns></returns>
+        Regex GetTableInnerTextRegex();
+        /// <summary>
+        /// 替换特殊标签
+        /// </summary>
+        /// <param name="entity">数据模型</param>
+        /// <param name="propName">innerText</param>
+        /// <returns>替换后的内容</returns>
+        string GetValue(object entity, string propName);
+    }
+
+    /// <summary>
+    /// 导出Docx文件的参数（默认实现）
+    /// </summary>
+    public class ExportDocxDefault : IExportDocx
+    {
+        public virtual string RangeSplitText { get; set; } = ":";
+        public string AttributeSplitText { get; set; } = "|";
+        public virtual string TextLeft { get; set; } = "{";
+        public virtual string TextRight { get; set; } = "}";
+        public virtual string TableTextLeft { get; set; } = "{{";
+        public virtual string TableTextRight { get; set; } = "}}";
+
+        protected Regex TextRegex;
+        public virtual Regex GetTextRegex()
+        {
+            if (TextRegex != null)
+            {
+                return TextRegex;
+            }
+
+            var left = TextLeft.GetPattern();
+            var right = TextRight.GetPattern();
+            var rangeSplitText = RangeSplitText.GetPattern();
+            var inner = "[^" + left + right + rangeSplitText + "]*?";
+            var pattern = left + inner + right;
+            TextRegex = new Regex(pattern);
+            return TextRegex;
+        }
+
+        protected Regex InnerTextRegex;
+        public virtual Regex GetInnerTextRegex()
+        {
+            if (InnerTextRegex != null)
+            {
+                return InnerTextRegex;
+            }
+
+            var left = TextLeft.GetPattern();
+            var right = TextRight.GetPattern();
+            var rangeSplitText = RangeSplitText.GetPattern();
+            var inner = "[^" + left + right + rangeSplitText + "]*?";
+            var pattern = "(?<=" + left + ")(" + inner + ")(?=" + right + ")";
+            InnerTextRegex = new Regex(pattern);
+            return InnerTextRegex;
+        }
+
+        protected Regex TableTextRegex;
+        public virtual Regex GetTableTextRegex()
+        {
+            if (TableTextRegex != null)
+            {
+                return TableTextRegex;
+            }
+
+            var left = TableTextLeft.GetPattern();
+            var right = TableTextRight.GetPattern();
+            var rangeSplitText = RangeSplitText.GetPattern();
+            var inner = ".*?" + rangeSplitText + ".*?";
+            var pattern = left + inner + right;
+            TableTextRegex = new Regex(pattern);
+            return TableTextRegex;
+        }
+
+        protected Regex TableInnerTextRegex;
+        public virtual Regex GetTableInnerTextRegex()
+        {
+            if (TableInnerTextRegex != null)
+            {
+                return TableInnerTextRegex;
+            }
+
+            var left = TableTextLeft.GetPattern();
+            var right = TableTextRight.GetPattern();
+            var rangeSplitText = RangeSplitText.GetPattern();
+            var inner = ".*?" + rangeSplitText + ".*?";
+            var pattern = "(?<=" + left + ")(" + inner + ")(?=" + right + ")";
+            TableInnerTextRegex = new Regex(pattern);
+            return TableInnerTextRegex;
+        }
+
+        public virtual string GetValue(object entity, string propName)
+        {
+            var value = entity.Getter(propName);
+            return value?.ToString();
+        }
+    }
+}
---
+++ DocxCore/RegexExtension.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Docx.Core
+{
+    public static class RegexExtension
+    {
+        /// <summary>
+        /// 获取所有匹配的字符串的Match
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="regex"></param>
+        /// <returns></returns>
+        public static List<Match> GetMatches(this string text, Regex regex)
+        {
+            var matches = regex.Matches(text);
+            return matches.ToList();
+        }
+        /// <summary>
+        /// 获取字符串中第一个匹配正则的InnerText
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="regex"></param>
+        /// <returns></returns>
+        public static string FirstMatchOrDefault(this string text, Regex regex)
+        {
+            var match = regex.Match(text);
+            if (!match.Success)
+            {
+                return string.Empty;
+            }
+
+            var innerText = match.Value;
+            return innerText;
+        }
+
+        /// <summary>
+        /// 获取转义后的正则表达式字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string GetPattern(this string str)
+        {
+            var result = string.Empty;
+            foreach (var s in str)
+            {
+                if (NeedEscape(s))
+                {
+                    result += @"\";
+                }
+
+                result += s.ToString();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 在正则表达式中是否需要转义
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool NeedEscape(this char s)
+        {
+            return s == '[' || s == ']' || s == '(' || s == ')' || s == '{' || s == '}' || s == '$' || s == '*' ||
+                   s == '+' || s == '.' || s == '?' || s == 92 || s == '^' || s == '|';
+        }
+
+        /// <summary>
+        /// 获取匹配列表字符串的所有字符串的Regex
+        /// </summary>
+        /// <param name="textList"></param>
+        /// <returns></returns>
+        public static Regex GetListRegex(this List<string> textList)
+        {
+            var pattern = string.Join("|", textList);
+            return new Regex(pattern);
+        }
+        /// <summary>
+        /// 获取匹配中间内容的Regex（非贪婪匹配）
+        /// </summary>
+        /// <param name="left">需要自行转义</param>
+        /// <param name="right">需要自行转义</param>
+        /// <param name="inner">需要自行转义</param>
+        /// <returns></returns>
+        public static Regex GetInnerTextRegex(string left, string right, string inner = "")
+        {
+            if (string.IsNullOrEmpty(inner))
+            {
+                inner = ".*?";
+            }
+
+            var pattern = "(?<=" + left + ")(" + inner + ")(?=" + right + ")";
+            var result = new Regex(pattern);
+            return result;
+        }
+
+        /// <summary>
+        /// 多组值同时替换（简单用Replace是不行的，比如Value1=Key2，那么Key1就被替换为Value2了）
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="dictionary"></param>
+        /// <returns></returns>
+        public static string ReplaceByDictionary(this string text, Dictionary<string, string> dictionary)
+        {
+            var regex = dictionary.Keys.ToList().GetListRegex();
+            var result = text.ReplaceByDictionary(dictionary, regex);
+            return result;
+        }
+
+        private static string ReplaceByDictionary(this string text, Dictionary<string, string> dictionary, Regex regex)
+        {
+            var match = regex.Match(text);
+            if (!match.Success)
+            {
+                return text;
+            }
+
+            var value = dictionary.FirstOrDefault(c => c.Key == match.Value).Value;
+            var cut = match.Index + match.Value.Length;
+            string result;
+            if (string.IsNullOrEmpty(value))
+            {
+                result = text.Substring(0, cut);
+            }
+            else
+            {
+                result = text.Substring(0, match.Index) + value;
+            }
+
+            result += text.Substring(cut).ReplaceByDictionary(dictionary, regex);
+            return result;
+        }
+    }
+}
